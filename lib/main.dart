581536988bf3a90c3f3b359c19abf16659974eda
Robin Sardja <robinsardja@gmail.com>
2023-11-18 19:17:29 -0500

    Created routes for Camera and Message.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp( const MainApp() ) ;
 }
 
+ThemeData theme = ThemeData.dark();
+
 class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
@@ -26,6 +28,7 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: theme,
       home: Scaffold(
         appBar: AppBar(
           title: const Center( child: Text("GDSnapChat") ),
@@ -36,8 +39,8 @@ class _MainAppState extends State<MainApp> {
             changeIndex(selectedIndex);
           },
           children: const [
-            Center( child: Text( "Camera" ), ),
-            Center( child: Text( "Message" ), ),
+            CameraPage(),
+            MessagePage(),
           ]
         ),
         bottomNavigationBar: BottomNavigationBar(
@@ -56,8 +59,52 @@ class _MainAppState extends State<MainApp> {
             changeIndex(selectedIndex);
             pageController.jumpToPage(selectedIndex);
           },
+          selectedFontSize: 0,
+          iconSize: 32,
         ),
       ),
+    );
+  }
+}
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const Scaffold(
+        body: Center(child: Text("Camera")),
+      )
+    );
+  }
+}
+
+class MessagePage extends StatefulWidget {
+  const MessagePage({super.key});
+
+  @override
+  State<MessagePage> createState() => _MessagePageState();
+}
+
+class _MessagePageState extends State<MessagePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      home: const Scaffold(
+        body: Center(child: Text("Message")),
+      )
     );
   }
 }
